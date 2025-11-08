c9ce3fcb8315937228f265299265999fbba18c6c
Mike <mikey@DESKTOP-3JM0ALA.localdomain>
2025-11-08 10:12:08 -0600

    4.Token Balance

--- test/TokenTest.js
+++ test/TokenTest.js
@@ -8,25 +8,34 @@ const tokens = (n) => {
 }
 
 describe("Token", () => {
+    const NAME = "Dapp University"
+    const SYMBOL = "DAPP"
+    const DECIMALS = 18
+    const TOTAL_SUPPLY = (tokens(1000000))
+
     it("Has correct Name", async () => {
         const { token } = await loadFixture(deployTokenFixture)
-        expect(await token.name()).to.equal("Dapp University")
+        expect(await token.name()).to.equal(NAME)
         
     })
     
     it("has correct Symbol", async () => {
         const { token } = await loadFixture(deployTokenFixture)
-        expect(await token.symbol()).to.equal("DAPP")
+        expect(await token.symbol()).to.equal(SYMBOL)
     })
 
     it("has correct Decimals", async () => {
         const { token } = await loadFixture(deployTokenFixture)
-        expect(await token.decimals()).to.equal(18)
+        expect(await token.decimals()).to.equal(DECIMALS)
     })
 
     it("has correct Total Supply", async () => {
         const { token } = await loadFixture(deployTokenFixture)
-        expect(await token.totalSupply()).to.equal(tokens(1000000))
+        expect(await token.totalSupply()).to.equal(TOTAL_SUPPLY)
     })
 
+    it("Assigns total Supply to Deployer", async () => {
+        const { token, deployer } = await loadFixture(deployTokenFixture)       
+        expect(await token.balanceOf(deployer.address)).to.equal(TOTAL_SUPPLY)
+    })
 })
