435e4ceafe7bb49e21ca61e3796b9d7938888913
Mike <mikey@DESKTOP-3JM0ALA.localdomain>
2025-11-09 14:22:25 -0600

    5. Transfer Tokens

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,6 +11,10 @@ contract Token {
     
     mapping(address => uint256) public balanceOf;
 
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    // event Transfer(address indexed _to, address indexed _from, uint _value);
+    
+
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
         name = _name;
         symbol = _symbol;
@@ -18,4 +22,18 @@ contract Token {
         balanceOf[msg.sender] = totalSupply;
     }
 
+    function transfer (address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Token: Insufficient Funds");
+        require(_to != address(0), "Token:Recipient is address 0");
+        
+        // Deduct tokens from sender
+        balanceOf[msg.sender] -= _value; 
+
+        // Credit tokens to recipient
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
 }
--- test/TokenTest.js
+++ test/TokenTest.js
@@ -8,7 +8,10 @@ const tokens = (n) => {
 }
 
 describe("Token", () => {
-    const NAME = "Dapp University"
+    
+    describe("Deployment", () => {
+
+        const NAME = "Dapp University"
     const SYMBOL = "DAPP"
     const DECIMALS = 18
     const TOTAL_SUPPLY = (tokens(1000000))
@@ -37,5 +40,57 @@ describe("Token", () => {
     it("Assigns total Supply to Deployer", async () => {
         const { token, deployer } = await loadFixture(deployTokenFixture)       
         expect(await token.balanceOf(deployer.address)).to.equal(TOTAL_SUPPLY)
+    })
+
+    })
+     
+    describe("Sending Tokens", () =>{
+        const AMOUNT = tokens(100)
+        
+        describe("Success", () => {
+            it("transfers a token balance", async () => {
+           const { token, deployer, receiver } = await loadFixture(deployTokenFixture) 
+
+           const transaction = await token.connect(deployer).transfer(receiver.address, AMOUNT)
+           await transaction.wait()
+
+           expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+           expect(await token.balanceOf(receiver.address)).to.equal(AMOUNT)
+            })
+
+        it("emits a transfer event", async () => {
+           const { token, deployer, receiver } = await loadFixture(deployTokenFixture) 
+
+           //Before transfer
+            const transaction = await token.connect(deployer).transfer(receiver.address, AMOUNT)
+            await transaction.wait()
+            
+            await expect(transaction).to.emit(token, "Transfer")
+            .withArgs(deployer.address, receiver.address, AMOUNT)
+            })
+        })
+
+        describe("Failure", () => {
+            it("It rejects insufficient balance", async () => {
+           const { token, deployer, receiver } = await loadFixture(deployTokenFixture) 
+
+           const INVALID_AMOUNT = tokens(1000000000)
+           const ERROR = "Token: Insufficient Funds"
+
+           await expect(token.connect(deployer).transfer(receiver.address, INVALID_AMOUNT))
+           .to.be.revertedWith(ERROR)
+            })
+        
+        it("Rejects invalid recipient", async () => {
+           const { token, deployer, receiver } = await loadFixture(deployTokenFixture) 
+
+           const INVALID_ADDRESS = "0x0000000000000000000000000000000000000000"
+           const ERROR = "Token:Recipient is address 0"
+
+           await expect(token.connect(deployer).transfer(INVALID_ADDRESS, AMOUNT))
+           .to.be.revertedWith(ERROR)
+
+            })
+        })
     })
 })
